c377b123b97af871b69391a1163983beda6fe8dc
otavio.costa <otavio.costa@CONTW-DES22.CONTMATIC.AD>
2017-05-04 11:46:05 -0300

    teste

---
+++ ProjetoOtavioMaven/src/test/java/br/com/contmatic/empresa/Main.java
@@ -0,0 +1,23 @@
+package br.com.contmatic.empresa;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class Main {
+
+    public static Result main(String[] args) {
+        
+        Result result = JUnitCore.runClasses(BairroTest.class, CidadeTest.class, EmpresaTest.class, EnderecoTest.class, EstadoTest.class, OperadoraTest.class, TelefoneTest.class);
+
+        for(Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        System.out.println(result.wasSuccessful());
+
+        return result;
+
+    }
+
+}
